7b54d3d612a1c17ad0d2eadbda722e4ad2d34fd4
SaudZubedi <SaudZubedi26@gmail.com>
2021-09-08 15:57:17 +0530

    Fixed SECRET_KEY error

--- myproject/settings.py
+++ myproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('MSG_APP_SECRET_KEY')
+SECRET_KEY = 'fOlb3o@@m5EaeqhtnoFw$EpNEcpnbkOu1i@XkZo!Csa!42n0f@Go5qU7?OkS2u5EYL@gKj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -156,10 +156,3 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
